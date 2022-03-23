654ba5026cc114c0cfbe1160327e5cbfea2e5239
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-23 18:58:59 +0530

    [Ankush] Add: Program for Bubble Sort

---
+++ DataStructureProblem/DataStructureProblem/BubbleSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblem
+{
+    class BubbleSort
+    {
+
+        public int[] arr;
+        public int Top;
+        public int numElements;
+
+        // constructor for bubble sort with parametr size
+        public BubbleSort(int size)
+        {
+            // storing int size in arr
+            arr = new int[size];
+            //top = size - 1
+            Top = size - 1;
+            // by default numElement is 0
+            numElements = 0;
+        }
+        /// insert array 
+        public void InsertNumbers(int item)
+        {
+
+            arr[numElements] = item;
+            numElements++;
+        }
+        /* Prints the array */
+        public void DisplayElements()
+        {
+            //loop if display = 0 display <= top value ; den increment by 1
+            for (int display = 0; display <= Top; display++)
+                // display array list ,
+                Console.WriteLine(arr[display] + ",");
+        }
+
+        ///method for  bubble sort 
+        public void BubbleSorts()
+        {
+            // instance variable
+            int temp;
+            //loop taking top value in outer ; outer > =1 ; outer +1
+            for (int outer = Top; outer >= 1; outer--)
+            {
+                //loop for inner is 0 ; inner < =1 ; inner +1
+                for (int inner = 0; inner <= outer - 1; inner++)
+                 if ((int)arr[inner] > arr[inner + 1])
+                    {
+                        // swap temp and arr[inner]
+                        temp = arr[inner];
+                        arr[inner] = arr[inner + 1];
+                        arr[inner + 1] = temp;
+                    }
+            }
+            //last display Elements
+            this.DisplayElements();
+        }
+    }
+}
--- DataStructureProblem/DataStructureProblem/Program.cs
+++ DataStructureProblem/DataStructureProblem/Program.cs
@@ -11,10 +11,19 @@ namespace DataStructureProblem
             {
                 Console.WriteLine("Enter Program number to get executed \n1.Binary search \n2. Insertion sort \n3.Bubble sort   \n4. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
-
-                InsertionSort.sort();
+               
+                //Bubble Sort
+                var Output = new BubbleSort(10);
+                Random random = new Random(20);
+                for (int i = 0; i < 10; i++)
+                    Output.InsertNumbers((int)(random.NextDouble() * 10));
+                Console.WriteLine("Before sorting:");
+                Output.DisplayElements();
+                Console.WriteLine("During sorting:");
+                Output.BubbleSorts();
+                Console.WriteLine("After sorting:");
+                Output.DisplayElements();
                 Console.ReadKey();
-
             }
       }
   }
