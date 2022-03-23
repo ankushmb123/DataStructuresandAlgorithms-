5006744e3b1e50a0e9f235bc474009fd14e033af
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-23 19:36:32 +0530

    [Ankush] Add :Program for Anagram

--- DataStructureProblem/DataStructureProblem/Program.cs
+++ DataStructureProblem/DataStructureProblem/Program.cs
@@ -9,20 +9,12 @@ namespace DataStructureProblem
          bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter Program number to get executed \n1.Binary search \n2. Insertion sort \n3.Bubble sort   \n4. Exit");
+                Console.WriteLine("Enter Program number to get executed \n1.Binary search \n2. Insertion sort \n3.Bubble sort \n4. Anagram Detection  \n5. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
-               
-                //Bubble Sort
-                var Output = new BubbleSort(10);
-                Random random = new Random(20);
-                for (int i = 0; i < 10; i++)
-                    Output.InsertNumbers((int)(random.NextDouble() * 10));
-                Console.WriteLine("Before sorting:");
-                Output.DisplayElements();
-                Console.WriteLine("During sorting:");
-                Output.BubbleSorts();
-                Console.WriteLine("After sorting:");
-                Output.DisplayElements();
+
+                //Anagram  
+                Anagramdetection anagram = new Anagramdetection();
+                anagram.Check();
                 Console.ReadKey();
             }
       }
