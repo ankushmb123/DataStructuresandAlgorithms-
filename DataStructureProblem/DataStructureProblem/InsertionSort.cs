f1ee4264e3ec6b254e6bf13c9f3f0d920b107720
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-23 18:48:25 +0530

    [Ankush] Add: Insertion Sort

---
+++ DataStructureProblem/DataStructureProblem/InsertionSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblem
+{
+    class InsertionSort
+    {
+        private static int j;
+
+        public static void sort()
+        {
+            // string array 
+            string[] Name_list = { "Shreya", "Prajakta", "Shubham", "Arun", "Pallavi", "Yogesh" };
+            //storing array value in insert sort method
+            InsertSort(Name_list);
+            //foreachloop
+            foreach (var name_item in Name_list)
+                Console.WriteLine(name_item);
+        }
+        /// IComparable interface so that I can sort in ascending or descending order and by different
+        static void InsertSort(IComparable[] array)
+        {
+            // instance variablex
+            int array_i, array_j;
+            /*storing current element whose left side is checked for its 
+          correct position .*/
+            for (array_i = 1; array_i < array.Length; array_i++)
+            {
+                /* check whether the adjacent element in left side is greater or
+                  less than the current element.  */
+                IComparable value = array[array_i];
+                array_j = array_i - 1;
+                /* check whether the adjacent element in left side is greater or
+                  less than the current element.  */
+                while ((array_j >= 0) && (array[array_j].CompareTo(value) > 0))
+                {
+                    // moving the left side element to one position forward.
+                    array[array_j + 1] = array[j];
+                    array_j--;
+                }
+                // moving current element to its  correct position.
+
+                array[array_j + 1] = value;
+            }
+        }
+    }
+}
--- DataStructureProblem/DataStructureProblem/Program.cs
+++ DataStructureProblem/DataStructureProblem/Program.cs
@@ -11,19 +11,10 @@ namespace DataStructureProblem
             {
                 Console.WriteLine("Enter Program number to get executed \n1.Binary search \n2. Insertion sort \n3.Bubble sort   \n4. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
-               
-                string[] arrword = { "hello", "hi", "how", "are", "you" };
-                Console.WriteLine("Unsorted array");
 
-                BinarySearch binary = new BinarySearch();
-                binary.Display(arrword);
-                Console.WriteLine("Sorted array");
-                string[] sortedArr = binary.SortingArray(arrword);
-                binary.Display(sortedArr);
-                binary.Binary_search(sortedArr);
+                InsertionSort.sort();
+                Console.ReadKey();
 
-                 
-              
             }
       }
   }
